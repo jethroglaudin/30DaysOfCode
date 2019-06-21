1f81728f7a32b9696257ca73fb7f1b6664a7cfd4
Jethro Glaudin <jethroglaudin.student@careerdevs.com>
2019-06-20 23:14:02 -0400

    Day17: More Exceptions

---
+++ Day17MoreExceptions.js
@@ -0,0 +1,58 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+////// Code Below ////////
+class Calculator
+{
+    power(n, p) {
+        try {
+           if ((n < 0) || (p < 0)) {
+                throw "n and p should be non-negative"
+           }
+           else {
+                return (Math.pow(n, p))
+            }
+        }
+        catch (err) {
+            return err
+        }
+        
+    }
+
+    
+}
+
+///// Code above //////
+
+function main(){
+    var myCalculator=new Calculator();
+    var T=parseInt(readLine());
+    while(T-->0){
+        var num = (readLine().split(" "));
+        try{
+            var n=parseInt(num[0]);
+            var p=parseInt(num[1]);
+            var ans=myCalculator.power(n,p);
+            console.log(ans);
+        }
+        catch(e){
+            console.log(e);
+        }
+
+    }
+}
